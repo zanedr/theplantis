b658cdfcd507e3034f8b2bd588447b14e1c5ac9e
Zane Russell <zane@zanes-mbp.lan>
2021-03-30 21:25:51 -0600

    first update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section id="app" class="fl w-90">
+    <header class="relative bg-white z-2 fixed w-100 h4 ph3 pv3 pv4-ns ph4-m ph5-l">
+<!--      <nav class="f6 fw6 ttu tracked">-->
+<!--        <a class="link dim white dib mr3" href="#" title="Home">Home</a>-->
+<!--        <a class="link dim white dib mr3" href="#" title="About">About</a>-->
+<!--        <a class="link dim white dib mr3" href="#" title="Store">Store</a>-->
+<!--        <a class="link dim white dib" href="#" title="Contact">Contact</a>-->
+<!--      </nav>-->
+      <p class="ttu tc inconsolata font-green">09.23.20</p>
+      <p class="ttu tc inconsolata font-green">This is The Plant Site the data base of my plants and how I care for them.</p>
+    </header>
+    <span class="flex flex-wrap justify-start pa5 mt5" v-if="plantsArray.length">
+        <plant-card v-for="(plant, index) in plantsArray"
+                    :key="index"
+                    :plant-data="plant"></plant-card>
+    </span>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlantCard from "@/components/PlantCard";
+import { plantsArray } from "@/stubData/plantData";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PlantCard,
+  },
+  data() {
+    return {
+      plantsArray: plantsArray
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './styles/custom.scss';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/components/PlantCard.vue
+++ src/components/PlantCard.vue
@@ -1,19 +1,15 @@
 <template>
-  <a class="db center mw5 tc black link dim"
-     :title="`${commonName} - ${latinName}`"
-<!--     href="https://geo.itunes.apple.com/us/album/blonde/id1146195596?at=1l3vqFJ&mt=1&app=music"-->
-  >
-
-<!--    <img class="db ba b&#45;&#45;black-10"/-->
-<!--    alt="Frank Ocean Blonde Album Cover"-->
-<!--    src="https://s3-us-west-1.amazonaws.com/tachyonsio/img/Blonde-Frank_Ocean.jpeg"-->
-<!--    >-->
+  <a class="db center mw5 tc black link pa1"
+     :title="`${plantData.commonName} - ${plantData.scientificName}`">
+    <img v-if="plantData.image" class="db bg-white grow-3d"
+         :alt="`${plantData.commonName} - ${plantData.scientificName}`"
+         :src="require(`@/assets/images/${plantData.image}`)">
 
     <dl class="mt2 f6 lh-copy">
-      <dt class="clip">Title</dt>
-      <dd class="ml0">Blonde</dd>
-      <dt class="clip">Artist</dt>
-      <dd class="ml0 gray">Frank Ocean</dd>
+      <dt class="clip">{{ plantData.commonName }}</dt>
+      <dd class="ml0">{{ plantData.scientificName }}</dd>
+      <dt class="clip">{{ plantData.commonName }}</dt>
+      <dd class="ml0 gray">{{ plantData.scientificName }}</dd>
     </dl>
   </a>
 </template>
@@ -22,12 +18,11 @@
 export default {
   name: 'PlantCard',
   props: {
-    commonName: String,
-    latinName: String,
-  }
+    plantData: Object
+  },
 }
 </script>
 
-<style>
+<style scoped lang="sass">
 
 </style>
