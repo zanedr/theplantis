f843492131f4edf1947cc94ada51f2300905c045
Zane Russell <zane@zanes-mbp.lan>
2021-03-31 00:17:11 -0600

    who knows

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
 # build
@@ -9,7 +8,7 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
+# you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
@@ -20,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:zanedr/theplantis.git master:gh-pages
 
 cd -
--- dist/vue.config.js
+++ dist/vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: '/theplants/'
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/theplantis/'
+        : '/'
 }
